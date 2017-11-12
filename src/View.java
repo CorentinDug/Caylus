169015cd9fd8640f35757a2b91fa15980758ef64
Schnoeby <schnoeby@outlook.fr>
2017-11-12 16:52:15 +0100

    Ajout des icones provisoire et d'un panel pour les infos joueurs

--- src/View.java
+++ src/View.java
@@ -1,19 +1,19 @@
 import com.apple.eawt.Application;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+
+import static java.awt.Color.BLACK;
+import static java.awt.Color.CYAN;
+import static java.awt.Color.GRAY;
 
 /**
  * Created by Schnoeby on 11/11/2017.
  */
 public class View extends JFrame {
 
-    private JPanel jpPlateau;
-    private PanelPrinc jpP;
+    private Plateau jpPlateau;
+    private JPanel panelInfo;
     private Application app;
     private JLabel[] caseP;
     private ImageIcon[] imageCase;
@@ -24,48 +24,72 @@ public class View extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
-        this.setSize(700, 1000);
+        this.setSize(1000, 1000);
         this.setLocationRelativeTo(null);
         this.initImage();
-        this.createView();
+        this.initInfo();
+        this.initPLateau();
         this.setVisible(true);
 
         this.app = Application.getApplication();
 
     }
 
-    public void createView() {
-        jpPlateau = new PanelPrinc(700, 1000);
+    public void initInfo() {
+        panelInfo=new Info(350,700);
+
+    }
+
+
+    public void initPLateau() {
+
+        jpPlateau = new Plateau(1000, 1000);
         jpPlateau.setLayout(null);
 
         int x = 30;
         int y = 850;
         int decalColonne =90;
+        int decalLigne =70;
+        int j=0;
 
         caseP=new JLabel[34];
         for(int i=0;i<34;i++) {
             caseP[i] = new JLabel();
             caseP[i].setIcon(imageCase[i]);
             jpPlateau.add(caseP[i]);
-            if(i%6==0 && i!=0){
-                y-=70;
+            if(j%6==0 && j!=0){
+                y-=decalLigne;
             }
             caseP[i].setBounds(x, y, 70, 70);
-            if(i%6==0 && i!=0){
+            if(j%6==0 && j!=0){
                 decalColonne=-decalColonne;
-                y-=70;
+                y-=decalLigne;
+            }
+            if(j==27){
+                x+=3*decalColonne;
+                decalLigne =50;
+                j=4;
             }
             x+=decalColonne;
+            j++;
 
         }
+        jpPlateau.add(panelInfo);
+        panelInfo.setBounds(650,370,350,700);
         setContentPane(jpPlateau);
     }
     public void initImage(){
         imageCase=new ImageIcon[34];
         for(int i=0;i<34;i++)
             imageCase[i]= new ImageIcon("./res/img//Batiment/Vide.png");
-        imageCase[13]= new ImageIcon("./res/img//Batiment/mine.png");
-
+        imageCase[20]= new ImageIcon("./res/img//Batiment/Charpentier.png");
+        imageCase[21]= new ImageIcon("./res/img//Batiment/marché.png");
+        imageCase[28]= new ImageIcon("./res/img//Batiment/taverne2.png");
+        imageCase[29]= new ImageIcon("./res/img//Batiment/ecurie.png");
+        imageCase[30]= new ImageIcon("./res/img//Batiment/joute.png");
+        imageCase[31]= new ImageIcon("./res/img//Batiment/taverne.png");
+        imageCase[32]= new ImageIcon("./res/img//Batiment/banque.png");
+        imageCase[33]= new ImageIcon("./res/img//Batiment/porte.png");
     }
 
 
