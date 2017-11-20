4b0704216468e96c8412c4642a7a7fecf2865805
Sevven <benji.esco@hotmail.fr>
2017-11-20 09:34:01 +0100

    Creation of the source's folder of the 'batiment', need to be completed.

---
+++ src/Batiment/Batiment.java
@@ -0,0 +1,16 @@
+package Batiment;
+import javax.swing.*;
+import java.util.List;
+
+/**
+ * Created by Schnoeby on 11/11/2017.
+ */
+public class Batiment {
+    private String nom;
+
+    public Batiment(String nom){
+        this.nom=nom;
+    }
+
+
+}
