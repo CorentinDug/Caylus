06e7d92f1ae7435cd30b4af2a98fa65abb37fc7e
Zaerhova <julien.robert1998@gmail.com>
2017-12-18 19:46:15 +0100

    Page d'aide terminer?

--- src/Caylus/Batiment/Prestige/Cathedrale.java
+++ src/Caylus/Batiment/Prestige/Cathedrale.java
@@ -9,11 +9,11 @@ public class Cathedrale extends Batiment {
      */
     public Cathedrale(){
         super("Cathedrale");
-        coutConst[0]=3;
-        coutConst[1]=4;
-        coutConst[2]=0;
-        coutConst[3]=1;
-        coutConst[4]=0;
+        coutConst[0]=3; //or
+        coutConst[1]=4; //nourriture
+        coutConst[2]=0; //pierre
+        coutConst[3]=1; //bois
+        coutConst[4]=0; //tissus
         prestige=25;
     }
 }
